06f5a01d520b29e180dbc00df5cd9130fad939c0
Pawel Kaliszewski <kaliszewskipawel@o2.pl>
2020-03-04 20:05:44 +0100

    initial commit

---
+++ src/test/java/pl/pawe/_kaliszewski/waiter_organizer/WaiterOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawe._kaliszewski.waiter_organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaiterOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
